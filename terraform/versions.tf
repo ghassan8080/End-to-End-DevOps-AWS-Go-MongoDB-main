0e613e0cdb044d7bfdb3872c60a05e88095c3dc2
Ghassan <gh1assan@gmail.com>
2025-12-20 21:56:17 +0300

    chore(terraform): relax aws provider constraint to >= 6.23.0 for module compatibility

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    aws        = "~> 4.29"
+    aws        = ">= 6.23.0"
     kubernetes = "~> 2.10"
     helm       = "~> 2.5"
     google     = "~> 4.34"
